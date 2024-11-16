a896b17373f39f3eab0a3d6795a0e4fe04864e77
Aleksandr Slinkov <sanch971@mail.ru>
2024-11-16 17:52:01 +0300

    Files for jenkins were added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,55 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
+
+from dotenv import load_dotenv
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
+def browser_management(load_env):
     print("Browser_management fixture is used")
-    driver_options = webdriver.FirefoxOptions()
-    driver_options.page_load_strategy = 'eager'
 
-    browser.config.driver_options = driver_options
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     browser.config.window_height = '1920'
     browser.config.window_width = '1080'
     browser.config.base_url = 'https://demoqa.com'
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.page_load_strategy = 'eager'
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     yield
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser, selenoid_url=None)
 
     browser.quit()
